30576f462c236ec9cf4d8e10bc6c7e7775f8f637
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-03-30 11:18:20 +0600

    add aos animation

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://blood-donation-server-nu-two.vercel.app'
 })
 const useAxiosSecure = () => {
   const navigate = useNavigate()
