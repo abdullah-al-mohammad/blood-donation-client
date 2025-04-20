961540553271651a273f5e0be35fd1ac6e7f92ab
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-04-20 06:11:00 +0600

    home and blog design look trying solve

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -50,7 +50,7 @@ export const router = createBrowserRouter([
       {
         path: 'donationDetails/:id',
         element: <BloodDonationRequestDetails></BloodDonationRequestDetails>,
-        loader: ({ params }) => fetch(`http://localhost:5000/donations/${params.id}`)
+        loader: ({ params }) => fetch(`https://blood-donation-77604.web.app/donations/${params.id}`)
       },
       {
         path: 'blog',
@@ -59,7 +59,7 @@ export const router = createBrowserRouter([
       {
         path: 'details/:id',
         element: <BlogDetails></BlogDetails>,
-        loader: ({ params }) => fetch(`http://localhost:5000/blogs/${params.id}`)
+        loader: ({ params }) => fetch(`https://blood-donation-77604.web.app/blogs/${params.id}`)
       },
       {
         path: 'register',
@@ -82,7 +82,7 @@ export const router = createBrowserRouter([
       {
         path: 'updateProfile/:id',
         element: <PrivateRoute><UpdateProfile></UpdateProfile></PrivateRoute>,
-        loader: ({ params }) => fetch(`http://localhost:5000/users/${params.id}`)
+        loader: ({ params }) => fetch(`https://blood-donation-77604.web.app/users/${params.id}`)
       },
       // donor page route
       {
@@ -100,7 +100,7 @@ export const router = createBrowserRouter([
       {
         path: 'updateDonationRequest/:id',
         element: <DonorRoute><UpdateDonation></UpdateDonation></DonorRoute>,
-        loader: ({ params }) => fetch(`http://localhost:5000/donations/${params.id}`)
+        loader: ({ params }) => fetch(`https://blood-donation-77604.web.app/donations/${params.id}`)
       },
       // Admin route page
       {
@@ -126,7 +126,7 @@ export const router = createBrowserRouter([
       {
         path: 'updateContent/:id',
         element: <AdminRoute><UpdateContent></UpdateContent></AdminRoute>,
-        loader: ({ params }) => fetch(`http://localhost:5000/blogs/${params.id}`)
+        loader: ({ params }) => fetch(`https://blood-donation-77604.web.app/blogs/${params.id}`)
       },
 
       // Volunteer route page
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://blood-donation-77604.web.app'
 })
 const useAxiosSecure = () => {
   const navigate = useNavigate()
